7272c4407f12296b938bc409c414ba179cf9791d
Philip Manke <philip.manke@hs-augsburg.de>
2016-07-20 11:18:50 +0200

    reverted change in threads wait behaviour

--- src/data_out.c
+++ src/data_out.c
@@ -18,12 +18,12 @@ static void *data_out_thread(void *arg) {
   pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
   inf->spinlock = &mutex;
   while (1) {
-    for (dcnt = 0; dcnt < inf->delay / 1000; dcnt++) {
+    for (dcnt = 0; dcnt < 30; dcnt++) {
       // this thread should have a high idle time but can only be stopped when
       // active.
       // therefore wake the thread every second during idle time to check if it
       // should stop.
-      sleep_milliseconds(1000);
+      sleep_milliseconds(inf->delay / 30);
       // lock mutex protecting the variable "running"
       pthread_mutex_lock(&mutex);
       if (!inf->running) { // stop the thread
